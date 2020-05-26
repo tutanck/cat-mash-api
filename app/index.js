e9614081471b3fe8043154770b07dff13e625486
tutanck <tutanck@gmail.com>
2020-05-26 17:31:00 +0200

    api base

---
+++ app/index.js
@@ -0,0 +1,38 @@
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { blue, green } = require('chalk');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', async () => {
+    console.log(green('Mongoose is connected to Mongodb.'));
+
+    app.listen(process.env.PORT);
+    console.log(blue(`Express ready !`));
+  });
+}
+
+module.exports = app;
